8a3bfaa34727e9a7dac87908e2168fc9e2bf641d
luiscarlos <luiscarlos.sf@outlook.com>
2018-10-26 01:40:42 -0300

    Adição da tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { InitialPage } from '../pages/initial/initial';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +13,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = InitialPage; //Deve ser a tela de login
 
   pages: Array<{title: string, component: any}>;
 
