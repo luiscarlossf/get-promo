5705b713cf79d7732ebc9ed11ba17f59c0d5f68b
luiscarlossf <luiscarlos.sf@outlook.com>
2018-10-31 14:54:11 -0300

    Corrigindo definições de classes

--- src/users/Anunciante.ts
+++ src/users/Anunciante.ts
@@ -4,9 +4,6 @@ class Anunciante extends Usuario{
 
     constructor(nome: string, apelido: string, email: string, interesses: number[], categorias: number[]) {
         super(nome,apelido,email,interesses)
-        this.nome = nome;
-        this.apelido = apelido;
-        this.email = email;
         this.categorias = [1,2,3]
     }
 
--- src/users/Usuario.ts
+++ src/users/Usuario.ts
@@ -4,10 +4,11 @@ class Usuario {
     email: string;
     interesses: number[];
 
-    constructor(nome: string, apelido: string, email: string, interesses: number[]) {
+    constructor(nome: string, apelido: string, email: string, senha: string, interesses: number[]) {
         this.nome = nome;
         this.apelido = apelido;
         this.email = email;
+        this.senha = senha;
         this.interesses = [1,2,3]
     }
 
