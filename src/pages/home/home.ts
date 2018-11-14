1e1822a30d5a036877129bac73c213343ea8ca2a
MatheusAlves98 <matheus-alves101@live.com>
2018-11-13 22:54:58 -0200

    issue_#29

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  anuncios: Array<any> = [
+    {
+      titulo:"Promocao de FastFood",
+      descricao :"Venha aproveitar o melhor fastfood da cidade com promocao especial.",
+      src: "assets/imgs/Fast-Food.jpg" 
+    },
+    {
+      titulo:"Black Friday na PDG",
+      descricao :"Aproveite a Black Friday para se livrar do aluguel e compre um imovel com descontos imperdiveis.",
+      src: "assets/imgs/pdg.jpg" 
+    },
+    {
+      titulo:"Carros 2015 e na SP Japan",
+      descricao :"Precos imperdiveis e condicoes especiais para voce andar de carro novo.",
+      src: "assets/imgs/carros.jpg" 
+    }
+  ]
+
   constructor(public navCtrl: NavController) {
 
   }
