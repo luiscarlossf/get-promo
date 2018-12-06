374752db9fad8ee9676fc7848940d6a8e02fac47
Luis Carlos <luiscarlos.sf@outlook.com>
2018-12-06 13:08:56 -0300

    Correção de erro na remoção do Usuário

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -48,7 +48,7 @@ export class UserProvider {
   public delete_user(apelido_: string){
     console.log('Deletendo');
 
-    this.http.delete("http://192.168.0.21:8080/usuario/deleteUsuario/", {params:{apelido:apelido}})
+    this.http.delete("http://192.168.0.21:8080/usuario/deleteUsuario/", {params:{apelido:apelido_}})
       .subscribe(data => {
         console.log(data);
        }, error => {
